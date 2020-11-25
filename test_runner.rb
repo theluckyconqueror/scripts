e06e0d647bf5be5652ff3db2659203d69d187dbb
Lucky Conqueror <theluckyconqueror@gmail.com>
2020-11-24 19:07:52 -0800

    Add really basic test runner script

---
+++ test_runner.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+require 'digest'
+require 'fileutils'
+
+test_file = ARGV[0]
+command = "ruby --verbose #{test_file}"
+
+if File.exist?(test_file)
+  old_hash = Digest::MD5.hexdigest(File.read(test_file))
+  while true
+    sleep(2)
+    new_hash = Digest::MD5.hexdigest(File.read(test_file))
+    if old_hash != new_hash
+      system("clear")
+      system(command)
+      old_hash = new_hash
+    else
+    end
+  end
+else
+  raise "File does not exist."
+end
