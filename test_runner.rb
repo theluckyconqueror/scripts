1458b66495ea843daaaf7525b3b69bd52299fa83
Lucky Conqueror <theluckyconqueror@gmail.com>
2020-11-30 18:22:07 -0800

    Change the file  we monitor for changes
    Once the tests are written that file doesn't change much if at all.
    It makes more sense to run the tests every time the application being tested gets saved.

--- test_runner.rb
+++ test_runner.rb
@@ -2,13 +2,14 @@
 require 'digest'
 
 test_file = ARGV[0]
+ruby_file = ARGV[1]
 command = "ruby --verbose #{test_file}"
 
-if File.exist?(test_file)
-  old_hash = Digest::MD5.hexdigest(File.read(test_file))
+if File.exist?(ruby_file)
+  old_hash = Digest::MD5.hexdigest(File.read(ruby_file))
   while true
     sleep(2)
-    new_hash = Digest::MD5.hexdigest(File.read(test_file))
+    new_hash = Digest::MD5.hexdigest(File.read(ruby_file))
     if old_hash != new_hash
       system("clear")
       system(command)
